2fe4d98a5b040a17fdc6cc9ff3d3822eefaed509
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2021-12-16 20:14:50 -0300

    Criação das Classes de domínio

---
+++ src/main/java/com/marcelo/osapi/domain/Cliente.java
@@ -0,0 +1,13 @@
+package com.marcelo.osapi.domain;
+
+public class Cliente extends Pessoa {
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+}
---
+++ src/main/java/com/marcelo/osapi/domain/Tecnico.java
@@ -0,0 +1,13 @@
+package com.marcelo.osapi.domain;
+
+public class Tecnico extends Pessoa {
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+}
