410ebb74be00baf74bf4b267101bbff421183d78
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2021-12-16 19:45:34 -0300

    Projeto criado.

---
+++ src/main/java/com/marcelo/osapi/OsapiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.osapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo/osapi/OsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.osapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
